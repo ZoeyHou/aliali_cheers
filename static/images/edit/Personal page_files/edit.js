ae46f05962a80915a8049c8641e3b3bb7e8fdab1
ZoeyHou <775165679@qq.com>
2016-11-25 00:58:15 +0800

    Add files via upload

---
+++ static/images/edit/Personal page_files/edit.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function(){
+
+	$("#Edit_img").click(function(){
+		document.getElementById("Upload_image").style.visibility="visibile";
+	});
+	$("#Upload_image_close").click(function(){
+		document.getElementById("Upload_image").style.visibility="hidden";
+	});
+});
