c8ef3a1344998048ed58c7b0e4bff1a2fb806a61
ZoeyHou <775165679@qq.com>
2016-11-25 00:54:14 +0800

    网页基本跳转完成
    1. 修改了页眉页脚，可以正常跳转，部分页面的一些忘改的地方也改了
    2. 有的页面元素的链接href没有改，但是因为不知道要改成什么，到时候搜索关键字html就出来了
    3. signup页面可以更改头像了，下面按钮改成Next，想跳转到signup2，但不知道改成什么所以跳到主页
    4. 加入了signup2的页面，js和css也根据django把src和href做了相应调整
    5. search页面发现js里有带图片，修改了一下src

---
+++ static/js/previous11.25/forget.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+  $("#forget_email_input").blur(function(){
+    if(document.getElementById("forget_email_input").value.length == 0){
+      document.getElementById("forget_email_hint").style.visibility="visible";
+    }else{
+      document.getElementById("forget_email_hint").style.visibility="hidden";
+    }
+  });
+});
+
+
--- static/js/previous11.25/header.js
+++ static/js/previous11.25/header.js
@@ -56,7 +56,6 @@ $(document).ready(function(){
 	});
   $("#Login_body").click(function(){
     document.getElementById("Face_login_page").style.visibility="hidden";
-    document.getElementById("Face_login_bgd").style.visibility="hidden";
     $("#Login_body").fadeOut(1000);
     $("#Login_containt").fadeOut(1000);
   });
@@ -64,7 +63,6 @@ $(document).ready(function(){
     $.post("/login/", {username: $("#email_input").val(), password: $("#pwd_input").val()}, function(ret){
     if(ret=='T'){
       document.getElementById("Face_login_page").style.visibility="hidden";
-      document.getElementById("Face_login_bgd").style.visibility="hidden";
       document.getElementById("Search_login").style.visibility="hidden";
       document.getElementById("User_name").innerHTML = getCookie("username")
       document.getElementById("User").style.visibility="visible";
@@ -78,17 +76,14 @@ $(document).ready(function(){
 
   $("#Face_button").click(function(){
     document.getElementById("Face_login_page").style.visibility="visible";
-    document.getElementById("Face_login_bgd").style.visibility="visible";
   });
 
   $("#Face_login_close").click(function(){
     document.getElementById("Face_login_page").style.visibility="hidden";
-    document.getElementById("Face_login_bgd").style.visibility="hidden";
   });
 
   $("#Face_login_button").click(function(){
     document.getElementById("Face_login_page").style.visibility="hidden";
-    document.getElementById("Face_login_bgd").style.visibility="hidden";
     $("#Login_body").fadeOut(1000);
     $("#Login_containt").fadeOut(1000);
     document.getElementById("Search_login").style.visibility="hidden";
@@ -100,75 +95,6 @@ $(document).ready(function(){
     document.getElementById("User").style.visibility="hidden";
   });
   });
-
-var pos = 0, up_url = "http://127.0.0.1:8000/login/recog_login";
-  var canvas = document.createElement("canvas");
-  canvas.setAttribute('width', 320);
-  canvas.setAttribute('height', 240);
-
-jQuery("#webcam").webcam({
-    width: 320,
-    height: 240,
-    mode: "callback",
-    swffile: "/static/js/jscam_canvas_only.swf", // canvas only doesn't implement a jpeg encoder, so the file is much smaller
-    onTick: function(remain) {
-        if (0 == remain) {
-            jQuery("#status").text("Cheese!");
-        } else {
-            jQuery("#status").text(remain + " seconds remaining...");
-        }
-    },
-
-    onSave: function(data) {
-  　　if (canvas.toDataURL) {
-    ctx = canvas.getContext("2d");
-    image = ctx.getImageData(0, 0, 320, 240);
-       
-    var col = data.split(";");
-    var img = image;
-  　　  for(var i = 0; i < 320; i++) {
-      　　var tmp = parseInt(col[i]);
-      　　img.data[pos + 0] = (tmp >> 16) & 0xff;
-      　　img.data[pos + 1] = (tmp >> 8) & 0xff;
-      　　img.data[pos + 2] = tmp & 0xff;
-      　　img.data[pos + 3] = 0xff;
-      　　pos+= 4;
-  　　  }
-    　　if (pos >= 4 * 320 * 240) {
-    　　  ctx.putImageData(img, 0, 0);
-    　　  $.post(up_url, {type: "data", image: canvas.toDataURL("image/jpeg"), username: $("#username").val()}, function(){
-    　　    location.reload();
-    　　  });
-    　　  pos = 0;
-    　　}　  
-  　　} else {
-      　　image.push(data);
-      　　pos+= 4 * 320;
-      　　
-      　　if (pos >= 4 * 320 * 240) {
-        　　$.post(up_url, {type: "data", image: image.join('|')},  function(){
-        　　location.reload();
-        　　});
-        　　pos = 0;
-      　　}　　
-  　　}
-    },
-
-   onCapture: function () {
-        webcam.save();
-   },
-
-   debug: function (type, string) {},
-
-   onLoad: function () {
-        var cams = webcam.getCameraList();
-        for(var i in cams) {
-            jQuery("#cams").append("<li>" + cams[i] + "</li>");
-        }
-   }
 });
-
-});
-
 
 
